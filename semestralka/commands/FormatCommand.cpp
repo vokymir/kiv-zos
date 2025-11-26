5c3ffb030eaa47c164229b3938cd71692c7477e1
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-11-26 10:32:30 +0100

    sem: format working on

--- semestralka/commands/FormatCommand.cpp
+++ semestralka/commands/FormatCommand.cpp
@@ -1,9 +1,52 @@
-#include <iostream>
+#include <cctype>
+#include <cstddef>
+#include <stdexcept>
 #include <string>
+#include <tuple>
 
 #include "commands.hpp"
 
 namespace jkfs {
+
+// ===== LOCAL =====
+
+// File size that the file can be formatted to.
+enum class FILE_SIZE {
+  B,
+  KB,
+  MB,
+  GB,
+};
+
+// convert from string to enum, optionally can start parsing string on position
+// greater than 0
+FILE_SIZE convert_filesize(std::string &str, size_t pos = 0) {
+  char ch;
+  ch = static_cast<char>(tolower(str[pos]));
+  switch (ch) {
+  case 'b':
+    return FILE_SIZE::B;
+  case 'k':
+    return FILE_SIZE::KB;
+  case 'm':
+    return FILE_SIZE::MB;
+  case 'g':
+    return FILE_SIZE::GB;
+  default:
+    throw std::logic_error("The size is not one of known (e.g. KB, MB,...)");
+  }
+}
+
+// from string, e.g. 600MB convert to 600 and MB
+std::tuple<int, FILE_SIZE> get_size(std::string &arg) {
+  size_t pos = 0;
+  int number = std::stoi(arg, &pos);
+  FILE_SIZE size = convert_filesize(arg, pos);
+
+  return {number, size};
+}
+
+// ===== GLOBAL =====
 
 FormatCommand::FormatCommand() {
   id_ = "format";
@@ -15,13 +58,10 @@ FormatCommand::FormatCommand() {
   how_ = "format 600MB // format the file to be filesystem of 600 Mebibyte";
 }
 
-void FormatCommand::execute(std::vector<std::string> &a_args) {
-  std::string args = "";
-  for (auto arg : a_args) {
-    args += arg + " ";
-  }
-  std::cout << "Running " + name_ + " command, with arguments: " << args
-            << std::endl;
+void FormatCommand::execute(std::vector<std::string> &args) noexcept {
+  int number;
+  FILE_SIZE size;
+  std::tie(number, size) = get_size(args[0]);
 }
 
 } // namespace jkfs
