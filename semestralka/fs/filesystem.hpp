8fb46d0747f28adcac9b71139fbdd3104d1dbc28
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-11-26 19:25:14 +0100

    introduce filesystem class

--- semestralka/fs/filesystem.hpp
+++ semestralka/fs/filesystem.hpp
@@ -1,36 +1,21 @@
-#include <cstdint>
+#include <filesystem>
+#include <fstream>
+#include <optional>
+#include <string>
 
 namespace jkfs {
 
-constexpr int32_t ITEM_FREE = 0;
+class Filesystem {
+private:
+  std::filesystem::path path_;
+  std::optional<std::fstream> file_;
 
-struct superblock {
-  char signature[16]; // author login
-  int32_t disk_size;  // celkova velikost VFS
-  int32_t cluster_size;
-  int32_t cluster_count;
-  int32_t bitmapi_start_addr;
-  int32_t bitmapd_start_addr;
-  int32_t inode_start_addr;
-  int32_t data_start_addr;
-};
+public:
+  Filesystem();
 
-struct inode {
-  int32_t node_id; // if == ITEM_FREE, inode is not used
-  char is_dir;
-  int32_t file_size;
-  int32_t direct1;
-  int32_t direct2;
-  int32_t direct3;
-  int32_t direct4;
-  int32_t direct5;
-  int32_t indirect1;
-  int32_t indirect2;
-};
-
-struct dir_item {
-  int32_t inode;
-  char item_name[12]; // 8 + 3 + \0
+  // get/set path
+  std::string path();
+  void path(std::string path);
 };
 
 } // namespace jkfs
---
+++ semestralka/fs/structures.hpp
@@ -0,0 +1,36 @@
+#include <cstdint>
+
+namespace jkfs {
+
+constexpr int32_t ITEM_FREE = 0;
+
+struct superblock {
+  char signature[16]; // author login
+  int32_t disk_size;  // celkova velikost VFS
+  int32_t cluster_size;
+  int32_t cluster_count;
+  int32_t bitmapi_start_addr;
+  int32_t bitmapd_start_addr;
+  int32_t inode_start_addr;
+  int32_t data_start_addr;
+};
+
+struct inode {
+  int32_t node_id; // if == ITEM_FREE, inode is not used
+  char is_dir;
+  int32_t file_size;
+  int32_t direct1;
+  int32_t direct2;
+  int32_t direct3;
+  int32_t direct4;
+  int32_t direct5;
+  int32_t indirect1;
+  int32_t indirect2;
+};
+
+struct dir_item {
+  int32_t inode;
+  char item_name[12]; // 8 + 3 + \0
+};
+
+} // namespace jkfs
