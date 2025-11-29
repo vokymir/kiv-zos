f70587bc73217e03d6d28dd44e7a3c32ddb65513
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-11-29 19:01:03 +0100

    dir_item_add()

--- semestralka/src/filesystem/dir.cpp
+++ semestralka/src/filesystem/dir.cpp
@@ -6,7 +6,7 @@
 
 namespace jkfs {
 
-void Filesystem::dir_create(int32_t parent_inode, std::string name) {
+void Filesystem::dir_create(int32_t parent_id, std::string name) {
   int32_t id = -1;
   int32_t cluster = -1;
   bool parent_entry_added = false;
@@ -32,17 +32,17 @@ void Filesystem::dir_create(int32_t parent_inode, std::string name) {
     inode_write(id, dir);
 
     // add this dir to parent
-    dir_item_add(parent_inode, id, name);
+    dir_item_add(parent_id, id, name);
     parent_entry_added = true;
 
     // add self, parent references
     dir_item_add(id, id, ".");
-    dir_item_add(id, parent_inode, "..");
+    dir_item_add(id, parent_id, "..");
 
   } catch (...) {
     // rollback
     if (parent_entry_added) {
-      dir_item_remove(parent_inode, name);
+      dir_item_remove(parent_id, name);
     }
     if (cluster >= 0) {
       cluster_free(cluster);
@@ -54,6 +54,19 @@ void Filesystem::dir_create(int32_t parent_inode, std::string name) {
     // let others know about the exception
     throw;
   }
+}
+
+void Filesystem::dir_item_add(int32_t id, int32_t item_id,
+                              std::string item_name) {
+  auto existing = dir_lookup(id, item_name);
+  if (existing >= 0) {
+    throw jkfilesystem_error("File with that name already exists.");
+  }
+
+  struct dir_item item{item_id, item_name};
+
+  auto offset = inode_read(id).file_size;
+  file_write(id, offset, reinterpret_cast<const char *>(&item), sizeof(item));
 }
 
 } // namespace jkfs
