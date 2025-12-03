bc082afaf7d805c20ea2e04bb7154d7cc5d84f98
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-12-03 09:01:42 +0100

    CatCommand

--- semestralka/src/commands/CatCommand.cpp
+++ semestralka/src/commands/CatCommand.cpp
@@ -1,7 +1,9 @@
+#include <ios>
 #include <iostream>
 #include <string>
 
 #include "commands.hpp"
+#include "errors.hpp"
 
 namespace jkfs {
 
@@ -12,13 +14,21 @@ CatCommand::CatCommand() {
   how_ = "cat file.txt";
 }
 
-void CatCommand::execute_inner(std::vector<std::string> &a_args) {
-  std::string args = "";
-  for (auto arg : a_args) {
-    args += arg + " ";
+void CatCommand::execute_inner(std::vector<std::string> &args) {
+  if (args.empty()) {
+    throw command_error(
+        "Cat command requires one argument - the name of file to concatenate.");
   }
-  std::cout << "Running " + name_ + " command, with arguments: " << args
-            << std::endl;
+
+  // find the file contents
+  auto path = args[0];
+  auto inode = fs_.path_lookup(path);
+  auto bytes = fs_.file_read(inode);
+
+  // write bytes to stdout
+  std::cout.write(reinterpret_cast<const char *>(bytes.data()),
+                  static_cast<std::streamsize>(bytes.size()));
+  std::cout << std::endl;
 }
 
 } // namespace jkfs
