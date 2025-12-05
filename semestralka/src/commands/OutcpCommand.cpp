88c8795542158e9db8088f471cfcfbf28daf0dc0
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-12-05 15:31:33 +0100

    outcp command

--- semestralka/src/commands/OutcpCommand.cpp
+++ semestralka/src/commands/OutcpCommand.cpp
@@ -1,7 +1,9 @@
-#include <iostream>
+#include <fstream>
+#include <ios>
 #include <string>
 
 #include "commands.hpp"
+#include "errors.hpp"
 
 namespace jkfs {
 
@@ -12,13 +14,33 @@ OutcpCommand::OutcpCommand() {
   how_ = "outcp inside_path.txt outside_path.txt";
 }
 
-void OutcpCommand::execute_inner(std::vector<std::string> &a_args) {
-  std::string args = "";
-  for (auto arg : a_args) {
-    args += arg + " ";
+void OutcpCommand::execute_inner(const std::vector<std::string> &args) {
+  if (args.size() < 2) {
+    throw command_error("The outcp command requires two arguments.");
   }
-  std::cout << "Running " + name_ + " command, with arguments: " << args
-            << std::endl;
+
+  auto data = read_unreal_file(args[0]);
+  write_real_file(args[1], data);
+}
+
+std::vector<uint8_t> OutcpCommand::read_unreal_file(const std::string &path) {
+  auto inode_id = fs_.path_lookup(path);
+
+  return fs_.file_read(inode_id);
+}
+
+void OutcpCommand::write_real_file(const std::string &path,
+                                   std::vector<uint8_t> &input) {
+  std::ofstream file(path, std::ios::binary);
+  if (!file) {
+    throw command_error("Cannot write into output file.");
+  }
+
+  file.seekp(0, std::ios::beg);
+  file.write(reinterpret_cast<const char *>(input.data()),
+             static_cast<std::streamsize>(input.size()));
+
+  file.close();
 }
 
 } // namespace jkfs
