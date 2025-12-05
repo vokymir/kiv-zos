7183256e22d2dc1de8d4066edb78e8ada29b7326
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-12-05 21:59:08 +0100

    fix auto/hexdump

--- semestralka/auto/hex.bash
+++ semestralka/auto/hex.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+file=tmp/filesystem.voky
+
 if [ "$1" == "0" ]; then
 
-hexdump -C filesystem.voky | awk '{ line=""; for(i=2;i<=17;i++) line=line $i; if(line !~ /^0+$/) print }'
+hexdump -C "$file" | awk '{ line=""; for(i=2;i<=17;i++) line=line $i; if(line !~ /^0+$/) print }'
 fi
 
 if [ "$1" == "1" ]; then
-xxd filesystem.voky | awk '$2 !~ /^00/ {print}'
+xxd "$file" | awk '$2 !~ /^00/ {print}'
 fi
