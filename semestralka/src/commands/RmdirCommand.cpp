b74307373e0336aca6e429f3b94c53838c8487b9
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-12-05 21:05:27 +0100

    rmdir command

--- semestralka/src/commands/RmdirCommand.cpp
+++ semestralka/src/commands/RmdirCommand.cpp
@@ -1,7 +1,8 @@
-#include <iostream>
 #include <string>
 
 #include "commands.hpp"
+#include "errors.hpp"
+#include "structures.hpp"
 
 namespace jkfs {
 
@@ -10,15 +11,30 @@ RmdirCommand::RmdirCommand() {
   name_ = "Remove directory";
   desc_ = "Remove directory if exists and is empty.";
   how_ = "rmdir empty_dir";
+
+  failure_message_ = "FILE NOT FOUND (neexistující adresář)";
 }
 
-void RmdirCommand::execute_inner(const std::vector<std::string> &a_args) {
-  std::string args = "";
-  for (auto arg : a_args) {
-    args += arg + " ";
+void RmdirCommand::execute_inner(const std::vector<std::string> &args) {
+  if (args.empty()) {
+    throw command_error("rmdir command requires one argument");
   }
-  std::cout << "Running " + name_ + " command, with arguments: " << args
-            << std::endl;
+
+  auto path = fs_.path_lookup(args[0]);
+
+  auto last_inode = fs_.inode_read(path.back());
+  if (!last_inode.is_dir) {
+    throw command_error("Cannot remove file which is not a directory.");
+  }
+
+  // every directory
+  if (!fs_.dir_empty(last_inode.node_id)) {
+    failure_message_ = "NOT EMPTY (adresář obsahuje podadresáře, nebo soubory)";
+    throw command_error("Cannot remove: directory not empty.");
+  }
+
+  std::filesystem::path p(args[0]);
+  fs_.file_delete(path.back() - 1, p.filename());
 }
 
 } // namespace jkfs
