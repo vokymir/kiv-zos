4b15be6be1494452d489fad04267e1f984c6e52f
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-12-05 15:15:46 +0100

    ls command upgrades

--- semestralka/src/commands/LsCommand.cpp
+++ semestralka/src/commands/LsCommand.cpp
@@ -9,11 +9,25 @@ LsCommand::LsCommand() {
   id_ = "ls";
   name_ = "List directory";
   desc_ = "List all items in directory.";
-  how_ = "ls . // lists all items in current directory";
+  how_ = "ls // lists all items in current directory";
+  exmp_ = {"ls // cwd", "ls . // cwd", "ls subdir // subdir", "ls / // root",
+           "ls filename // will just print filename"};
 }
 
-void LsCommand::execute_inner([[maybe_unused]] std::vector<std::string> &_) {
-  auto cwd = fs_.path_lookup(".");
+void LsCommand::execute_inner(std::vector<std::string> &args) {
+  std::string path;
+  if (args.size() > 0) {
+    path = args[0];
+  }
+
+  auto cwd = fs_.path_lookup(path);
+  // mimicking linux behaviour on "ls file"
+  auto cwd_inode = fs_.inode_read(cwd);
+  if (!cwd_inode.is_dir) {
+    std::cout << path << std::endl;
+    return;
+  }
+
   auto items = fs_.dir_list(cwd);
 
   for (const auto &item : items) {
