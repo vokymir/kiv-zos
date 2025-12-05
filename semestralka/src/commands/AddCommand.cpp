5452d70fbadd55af1c6cc235568eb743fc099a0e
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-12-05 16:04:48 +0100

    add command

--- semestralka/src/commands/AddCommand.cpp
+++ semestralka/src/commands/AddCommand.cpp
@@ -1,7 +1,7 @@
-#include <iostream>
 #include <string>
 
 #include "commands.hpp"
+#include "errors.hpp"
 
 namespace jkfs {
 
@@ -12,13 +12,21 @@ AddCommand::AddCommand() {
   how_ = "add s1 s2 // append s2 to the end of file s1";
 }
 
-void AddCommand::execute_inner(const std::vector<std::string> &a_args) {
-  std::string args = "";
-  for (auto arg : a_args) {
-    args += arg + " ";
+void AddCommand::execute_inner(const std::vector<std::string> &args) {
+  if (args.size() < 2) {
+    throw command_error(
+        "Add command requires 2 arguments. See 'add -h' for more info.");
   }
-  std::cout << "Running " + name_ + " command, with arguments: " << args
-            << std::endl;
+
+  auto from_id = fs_.path_lookup(args[0]);
+  auto to_id = fs_.path_lookup(args[1]);
+
+  auto from_data = fs_.file_read(from_id);
+  auto to_inode = fs_.inode_read(to_id);
+
+  fs_.file_write(to_id, to_inode.file_size,
+                 reinterpret_cast<const char *>(from_data.data()),
+                 from_data.size());
 }
 
 } // namespace jkfs
